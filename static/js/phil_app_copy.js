d381af250659a9c3c22e9fadc21daea9af97fd2f
lilphonsdale <118135886+lilphonsdale@users.noreply.github.com>
2023-03-30 20:55:02 -0700

    sort of a working slider
    it does slide

--- static/js/phil_app_copy.js
+++ static/js/phil_app_copy.js
@@ -13,19 +13,19 @@ function init() {
       Object.entries(countryIDs).forEach(([k,v]) => {
       choices.append("option").attr("value", v).text(v)});
 
-      //Use the first sampleId to generate the first charts
+      //Use the first country to generate the first chart
 
     let firstCountry = countryIDs[0];
     visualize(firstCountry)
   }
   )};
 
-// make the charts
+// make the chart
 
 function visualize(country) {
    d3.json(report).then((data) => {
     let countryofInterest = data.filter(x => x.Country == country);
-    console.log(countryofInterest)
+    console.log(countryofInterest);
     years = []
     happinessScore = []
     family = []
@@ -39,51 +39,106 @@ function visualize(country) {
       generosity.push(countryofInterest[i].Generosity)
     };
 
-    var trace4 = {
-      type: "scatter",
-      mode: "lines",
-      name: 'Generosity',
-      x: years,
-      y: generosity,
-      line: {color: 'blue'}
-    }
+    // var trace4 = {
+    //   type: "scatter",
+    //   mode: "lines",
+    //   name: 'Generosity',
+    //   x: years,
+    //   y: generosity,
+    //   line: {color: 'blue'}
+    // }
 
-    var trace3 = {
-      type: "scatter",
-      mode: "lines",
-      name: 'Freedom',
-      x: years,
-      y: freedom,
-      line: {color: 'red'}
-    }
+    // var trace3 = {
+    //   type: "scatter",
+    //   mode: "lines",
+    //   name: 'Freedom',
+    //   x: years,
+    //   y: freedom,
+    //   line: {color: 'red'}
+    // }
 
-    var trace2 = {
-      type: "scatter",
-      mode: "lines",
-      name: 'Family',
-      x: years,
-      y: family,
-      line: {color: 'green'}
-    }
+    // var trace2 = {
+    //   type: "scatter",
+    //   mode: "lines",
+    //   name: 'Family',
+    //   x: years,
+    //   y: family,
+    //   line: {color: 'green'}
+    // }
   
-    var trace1 = {
-      type: "scatter",
-      mode: "lines",
-      name: 'Happiness Score',
-      x: years,
-      y: happinessScore,
-      line: {color: 'lightgrey'}
-    }
+    // var trace1 = {
+    //   type: "scatter",
+    //   mode: "lines",
+    //   name: 'Happiness Score',
+    //   x: years,
+    //   y: happinessScore,
+    //   line: {color: 'lightgrey'}
+    // }
 
-    var traces = [trace1, trace2, trace3, trace4]
+    // var traces = [trace1, trace2, trace3, trace4]
 
-    Plotly.newPlot('myDiv', traces)
+    // Plotly.newPlot('myDiv', traces)
 
-    });
-  // });
+  var trace2 = {
+    type: "scatter",
+    mode: "lines",
+    name: 'Happiness Score',
+    fill: 'tonexty',
+    x: years,
+    y: happinessScore,
+    line: {color: 'grey'}
+  }
+
+  var trace1 = {
+    type: "scatter",
+    mode: "lines",
+    name: 'Generosity',
+    x: years,
+    y: generosity,
+    line: {color: 'lightgrey'}
+  }
+
+  var data = [trace1,trace2];
+
+  var selectorOptions = {
+    buttons: [{
+        step: 'month',
+        stepmode: 'backward',
+        count: 1,
+        label: '1m'
+    }, {
+        step: 'month',
+        stepmode: 'backward',
+        count: 6,
+        label: '6m'
+    }, {
+        step: 'year',
+        stepmode: 'todate',
+        count: 1,
+        label: 'YTD'
+    }, {
+        step: 'year',
+        stepmode: 'backward',
+        count: 1,
+        label: '1y'
+    }, {
+        step: 'all',
+    }],
 };
 
+  var layout = {
+    title: 'Time series with range slider and selectors',
+    xaxis: {
+        rangeselector: selectorOptions,
+        rangeslider: {}
+    },
+    yaxis: {
+        fixedrange: true
+    }
+};
 
+Plotly.newPlot('myDiv', data, layout);
+   })};
 // // A function to update the charts when a selection is made from the dropdown menu
 
 function optionChanged(newSample) {
